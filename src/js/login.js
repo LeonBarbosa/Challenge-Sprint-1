60bed0ea05b40e76be9461949d6a0b1b598a7af3
Leonardo Barbosa <leofsb78@gmail.com>
2024-06-17 14:53:20 -0300

    Criando Repositorio

---
+++ src/js/login.js
@@ -0,0 +1,44 @@
+/*=============== login ===============*/
+
+/*===== FOCUS =====*/
+const inputs = document.querySelectorAll(".form__input")
+
+/*=== Add focus ===*/
+function addfocus(){
+    let parent = this.parentNode.parentNode
+    parent.classList.add("focus")
+}
+
+/*=== Remove focus ===*/
+function remfocus(){
+    let parent = this.parentNode.parentNode
+    if(this.value == ""){
+        parent.classList.remove("focus")
+    }
+}
+
+/*=== To call function===*/
+inputs.forEach(input=>{
+    input.addEventListener("focus",addfocus)
+    input.addEventListener("blur",remfocus)
+})
+
+/*===== LOGIN =====*/
+document.addEventListener('DOMContentLoaded', () => {
+  const showRegisterLink = document.getElementById('show-register');
+  const showLoginLink = document.getElementById('show-login');
+  const loginForm = document.getElementById('login-form');
+  const registerForm = document.getElementById('register-form');
+
+  showRegisterLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      loginForm.style.display = 'none';
+      registerForm.style.display = 'block';
+  });
+
+  showLoginLink.addEventListener('click', (e) => {
+      e.preventDefault();
+      registerForm.style.display = 'none';
+      loginForm.style.display = 'block';
+  });
+});
